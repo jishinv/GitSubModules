3a01c0010dabb8547f4398a83bfa5f7873dfe291
Persistent <persistent@Unicorns-MacBook-Pro-2.local>
2015-06-19 12:31:40 +0530

    Adding project

---
+++ GitSubModules/GitSubModules/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitSubModules
+//
+//  Created by Persistent on 19/06/15.
+//  Copyright (c) 2015 com.psl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitSubModules/GitSubModules/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitSubModules
+//
+//  Created by Persistent on 19/06/15.
+//  Copyright (c) 2015 com.psl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
